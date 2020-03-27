94f06f075812475d6dfbea6a5beec7103ca89edc
Michael Bijker <michael@microcapital.nl>
2020-03-27 20:04:39 +0100

    more basic stuff

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 /* eslint-env node, es6 */
-var express = require("express");
+const express = require("express");
+const path = require("path");
 
 var app = express();
 
 var server = app.listen (3000);
 
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
