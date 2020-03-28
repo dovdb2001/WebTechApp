e51e0035255f3b76a3881494304557ad2db0c405
Michael Bijker <michael@microcapital.nl>
2020-03-28 15:27:26 +0100

    very basic login in system

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 /* eslint-env node, es6 */
 const express = require("express");
 const path = require("path");
-
-var app = express();
+const app = express();
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
 var server = app.listen (3000);
+
+var users = [{id: '1585404001350', name: 'Michael', email: 'michael@gmail.com', password: '$2a$10$Dib4CtBAA6VN/ROBY4hK7er/Gkv6uPgyCQeRfPTur2A3jZ85KKfXe'}]; //password = 'pwd'
+
+app.use(express.urlencoded({extended: false}));
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 app.get("/main.css", (req, res) => {
@@ -13,17 +23,83 @@ app.get("/main.css", (req, res) => {
 
 
 app.get("/", (req, res) => {
+    console.log(req.session.user);
     res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
 app.get("/login", (req, res) => {
+    console.log(req.session.user);
     res.sendFile(path.join(__dirname, "/views/login.html"));
 });
 
 app.get("/register", (req, res) => {
+    console.log(req.session.user);
     res.sendFile(path.join(__dirname, "/views/register.html"));
 });
 
 app.get("/browse", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/browse.html"));
+    console.log(req.session.user);
+    if (req.session.user === 2020) {
+        res.sendFile(path.join(__dirname, "/views/browse.html"));
+    } else {
+        res.redirect("/");
+    }
 });
+
+app.get("/logout", (req, res) => {
+    req.session.user = undefined;
+    res.redirect("/")
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect("/login");
+    } catch {
+        res.redirect("/register");
+    }
+    console.log(users);
+});
+
+
+app.post("/login", async (req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.password);
+
+    const stored_pwd = (users.find(x => x.email === req.body.email)).password;
+    //const given_pwd = await bcrypt.hash(req.body.password, 10);
+
+    console.log(stored_pwd);
+    //console.log(given_pwd);
+
+    if (await bcrypt.compare(req.body.password, stored_pwd)) {
+        req.session.user = 2020;
+        res.redirect("/browse");
+    } else {
+        req.session.user = undefined;
+        res.redirect("/login");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
