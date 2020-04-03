1cea92686c92dadc947f636f440487818bdf0e09
Michael Bijker <michael@microcapital.nl>
2020-04-03 21:19:00 +0200

    course information added in basic form

--- public/javascript/course-details.js
+++ public/javascript/course-details.js
@@ -13,7 +13,7 @@ function getCourse() {
     req.open("GET", "/course-info/" + courseCode, true);
     req.onreadystatechange = function () {
         if (req.readyState == 4 && req.status == 200) {
-            course = JSON.parse(req.responseText);
+            course = JSON.parse(req.responseText)[0];
             drawCourse();
         }
     }
@@ -21,7 +21,17 @@ function getCourse() {
 }
 
 function drawCourse() {
+    var title = document.createElement("h1");
+    title.textContent = "Course details: " + course.code;
+    document.getElementById("table").before(title);
 
+    (document.getElementById("title")).append(document.createTextNode(course.title));
+    (document.getElementById("code")).append(document.createTextNode(course.code));
+    (document.getElementById("programme")).append(document.createTextNode(course.programme));
+    (document.getElementById("level")).append(document.createTextNode(course.level));
+    (document.getElementById("faculty")).append(document.createTextNode(course.faculty));
+    (document.getElementById("semester")).append(document.createTextNode(course.semester));
+    (document.getElementById("description")).append(document.createTextNode(course.description));
 }
 
 function enrollment() {
